ca800f77776114a5395acfed472d510ec91441e2
wdf <>
2019-04-28 22:41:47 +0800

    weather layout and activity

--- app/src/main/java/edu/monashsuzhou/friendfinder/Constant.java
+++ app/src/main/java/edu/monashsuzhou/friendfinder/Constant.java
@@ -4,4 +4,9 @@ public interface Constant {
     String SEVER_HOST = "192.168.6.12";
     String SEVER_PORT = "8080";
     String SEVER_NAME = "5183_A1";
+
+    String WEATHER_HOST = "http://api.openweathermap.org/data/2.5/forecast/daily";
+    String APPID = "79badf94102e008963c2d50b6cfa43f2";
+    String UNITS = "metric";
+    Integer N_DAYS = 1;
 }
--- app/src/main/java/edu/monashsuzhou/friendfinder/MainActivity.java
+++ app/src/main/java/edu/monashsuzhou/friendfinder/MainActivity.java
@@ -8,22 +8,32 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import edu.monashsuzhou.friendfinder.R;
+import edu.monashsuzhou.friendfinder.util.HttpUtil;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
 import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
 
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -36,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv_time;
     private TextView tv_temp;
     private TextView tv_city;
-    private TextView tv_discrip;
+    private TextView tv_welcome;
+    private TextView tv_descrip;
+    private ImageView iv_weather;
 
     private Handler mHandler;
     //定位都要通过LocationManager这个类实现
@@ -75,7 +87,13 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-        String location = "Suzhou";
+
+        //显示地点和温度
+        WeatherDisplayTask weatherDisplayTask = new WeatherDisplayTask();
+        weatherDisplayTask.execute("Nanjing", "Monny");
+
+
+
         //通过location去查天气情况
         //显示地点和温度
         //第一步：检索参数为城市和天气类型，实况天气为WEATHER_TYPE_LIVE、天气预报为WEATHER_TYPE_FORECAST
@@ -86,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 //            public void onWeatherLiveSearched(LocalWeatherLiveResult result, int i) {
 //                LocalWeatherLive liveResult = result.getLiveResult();
 ////            liveTimeTxt.setText(liveResult.getReportTime()); //时间
-//                tv_discrip.setText(liveResult.getWeather()); //气象
+//                tv_descrip.setText(liveResult.getWeather()); //气象
 //                tv_temp.setText(liveResult.getTemperature() + "°C");     //温度
 //            }
 //
@@ -104,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
 //        public void onWeatherLiveSearched(LocalWeatherLiveResult result, int i) {
 //            LocalWeatherLive liveResult = result.getLiveResult();
 ////            liveTimeTxt.setText(liveResult.getReportTime()); //时间
-//            tv_discrip.setText(liveResult.getWeather()); //气象
+//            tv_descrip.setText(liveResult.getWeather()); //气象
 //            tv_temp.setText(liveResult.getTemperature() + "°C");     //温度
 ////            liveWindDirectionTxt.setText(liveResult.getWindDirection()); //风向
 ////            liveWindPowerTxt.setText(liveResult.getWindPower() + "级"); //风力
@@ -122,7 +140,9 @@ public class MainActivity extends AppCompatActivity {
         tv_time = (TextView) findViewById(R.id.tv_time);
         tv_temp = (TextView) findViewById(R.id.tv_temp);
         tv_city = (TextView) findViewById(R.id.tv_city);
-        tv_discrip = (TextView) findViewById(R.id.tv_discrip);
+        tv_descrip = (TextView) findViewById(R.id.tv_descrip);
+        tv_welcome = (TextView) findViewById(R.id.tv_welcome);
+        iv_weather = (ImageView) findViewById(R.id.iv_weather);
 
     }
 
@@ -172,6 +192,75 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    public static int getImageIcon(String weatherDescription){
+        int weatherIcon ;
+        switch(weatherDescription) {
+            case "Thunderstorm":
+                weatherIcon = R.mipmap.ic_atmosphere;
+                break;
+            case "Drizzle":
+                weatherIcon = R.mipmap.ic_drizzle;
+                break;
+            case "Rain":
+                weatherIcon = R.mipmap.ic_rain;
+                break;
+            case "Snow":
+                weatherIcon = R.mipmap.ic_snow;
+                break;
+            case "Atmosphere":
+                weatherIcon = R.mipmap.ic_atmosphere;
+                break;
+            case "Clear":
+                weatherIcon = R.mipmap.ic_clear;
+                break;
+            case "Clouds":
+                weatherIcon = R.mipmap.ic_cloudy;
+                break;
+            case "Extreme":
+                weatherIcon = R.mipmap.ic_extreme;
+                break;
+            default:
+                weatherIcon = R.mipmap.ic_launcher;
+        }
+        return weatherIcon;
+
+    }
+
+    private class WeatherDisplayTask extends AsyncTask<String, Integer, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            String city = strings[0];
+            String uName = strings[1];
+            tv_city.setText(city);
+            tv_welcome.setText("Welcome, " + uName + "!");
+            try {
+                String weatherInfo = HttpUtil.get(Constant.WEATHER_HOST + "?q=" + city + "&APPID=" + Constant.APPID
+                        + "&units=" + Constant.UNITS + "&cnt=" + Constant.N_DAYS);
+                return weatherInfo;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String weatherInfo) {
+            super.onPostExecute(weatherInfo);
+            Log.i("Weather", weatherInfo);
+            JSONObject weatherJson = JSON.parseObject(weatherInfo);
+
+            tv_temp.setText("" + weatherJson.getJSONArray("list").getJSONObject(0)
+                    .getJSONObject("temp").getInteger("day") + "°");
+            tv_descrip.setText(weatherJson.getJSONArray("list").getJSONObject(0)
+                    .getJSONArray("weather").getJSONObject(0).getString("description"));
+            String icon = weatherJson.getJSONArray("list").getJSONObject(0)
+                    .getJSONArray("weather").getJSONObject(0).getString("main");
+            iv_weather.setImageResource(getImageIcon(icon));
+
+
+        }
+    }
 
 
 
