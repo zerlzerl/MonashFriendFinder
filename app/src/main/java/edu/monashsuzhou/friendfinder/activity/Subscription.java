025baa3b77eb431e0b93f8b2912f1ac066559caa
zerl <zerlzerl@163.com>
2019-04-28 12:58:21 +0800

    subscription course selector

--- app/src/main/java/edu/monashsuzhou/friendfinder/activity/Subscription.java
+++ app/src/main/java/edu/monashsuzhou/friendfinder/activity/Subscription.java
@@ -1,22 +1,27 @@
 package edu.monashsuzhou.friendfinder.activity;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import edu.monashsuzhou.friendfinder.R;
@@ -25,7 +30,12 @@ import edu.monashsuzhou.friendfinder.MainActivity;
 
 public class Subscription extends AppCompatActivity {
     private DatePickerDialog datePicker;
-    EditText dateOfBirth;
+    private EditText dateOfBirth;
+
+    private EditText courseSelector;
+    private ArrayList<Integer> mSelectedItems;
+    private boolean[] selectedItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +57,59 @@ public class Subscription extends AppCompatActivity {
                 datePicker = new DatePickerDialog(Subscription.this,
                         (view1, year1, monthOfYear, dayOfMonth) -> dateOfBirth.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1), year, month, day);
                 datePicker.show();
+            }
+        });
+
+        //set course selector
+        mSelectedItems = new ArrayList();  // Where we track the selected items
+        selectedItems = new boolean[getResources().getStringArray(R.array.units).length];
+        courseSelector = (EditText) findViewById(R.id.course_multi_selector);
+        courseSelector.setInputType(InputType.TYPE_NULL);
+        courseSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                Log.i(Subscription.class.getName(), "Click Course Selector");
+                AlertDialog.Builder builder = new AlertDialog.Builder(Subscription.this);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        String courseSelectorContent = "";
+                        for (Integer item : mSelectedItems){
+                            selectedItems[item] = true;
+                            courseSelectorContent += getResources().getStringArray(R.array.units)[item] + ",";
+                        }
+                        Log.i(Subscription.class.getName(), courseSelectorContent);
+                        ((TextView)view.findViewById(R.id.course_multi_selector)).setText(courseSelectorContent);
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                // 2. Chain together various setter methods to set the dialog characteristics
+//                selectedItems = new boolean[getResources().getStringArray(R.array.units).length];
+//                for (Integer item : mSelectedItems){
+//                    selectedItems[item] = true;
+//                }
+                builder.setTitle(R.string.course_select)
+                        .setMultiChoiceItems(R.array.units, selectedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which, boolean isChecked) {
+                                if (isChecked) {
+                                    // If the user checked the item, add it to the selected items
+                                    mSelectedItems.add(which);
+                                } else if (mSelectedItems.contains(which)) {
+                                    // Else, if the item is already in the array, remove it
+                                    mSelectedItems.remove(Integer.valueOf(which));
+                                }
+                            }
+                        });
+
+                // 3. Get the AlertDialog from create()
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
