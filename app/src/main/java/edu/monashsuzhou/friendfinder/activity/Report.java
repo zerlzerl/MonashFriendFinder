de3d347b3b7503be73e8827e6d2aeeb270db5ac7
amoy_zhp <whitehawk15@hotmail.com>
2019-05-02 10:14:24 +0800

    draw report

--- app/src/main/java/edu/monashsuzhou/friendfinder/activity/Report.java
+++ app/src/main/java/edu/monashsuzhou/friendfinder/activity/Report.java
@@ -10,11 +10,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import android.util.DisplayMetrics;
+import android.graphics.Color;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
 import edu.monashsuzhou.friendfinder.R;
 
 import edu.monashsuzhou.friendfinder.MainActivity;
 
 public class Report extends AppCompatActivity {
+    private PieChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +39,10 @@ public class Report extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.report_toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        mChart = (PieChart) findViewById(R.id.pie_chart);
+
+        PieData mPieData = getPieData(4, 100);
+        showChart(mChart, mPieData);
     }
 
     @Override
@@ -57,4 +69,98 @@ public class Report extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void showChart(PieChart pieChart, PieData pieData) {
+        pieChart.setHoleColor(Color.TRANSPARENT);
+        pieChart.setHoleRadius(50f);  //半径
+        pieChart.setTransparentCircleRadius(54f); // 半透明圈
+        //pieChart.setHoleRadius(0)  //实心圆
+        //pieChart.setDescription("测试饼状图");
+        // mChart.setDrawYValues(true);
+        pieChart.setDrawCenterText(true);  //饼状图中间可以添加文字
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setRotationAngle(90); // 初始旋转角度
+        pieChart.setCenterTextSize(15);
+        // draws the corresponding description value into the slice
+        // mChart.setDrawXValues(true);
+        // enable rotation of the chart by touch
+        pieChart.setRotationEnabled(true); // 可以手动旋转
+        // display percentage values
+        pieChart.setUsePercentValues(true);  //显示成百分比
+
+        pieChart.setCenterText("Quarterly Revenue");  //饼状图中间的文字
+        //设置数据
+        pieChart.setData(pieData);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        Legend l = mChart.getLegend();
+        l.setYOffset(12);
+        l.setXOffset(12);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER );
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+        pieChart.animateXY(1000, 1000);  //设置动画
+        // mChart.spin(2000, 0, 360);
+    }
+
+    private PieData getPieData(int count, float range) {
+        List<String> xValues = new ArrayList<String>();  //xVals用来表示每个饼块上的内容
+        for (int i = 0; i < count; i++) {
+            xValues.add("Quarterly" + (i + 1));  //饼块上显示成Quarterly1, Quarterly2, Quarterly3, Quarterly4
+        }
+        List<PieEntry> yValues = new ArrayList<PieEntry>();  //yVals用来表示封装每个饼块的实际数据
+        // 饼图数据
+        /**
+         * 将一个饼形图分成四部分， 四部分的数值比例为14:14:34:38
+         * 所以 14代表的百分比就是14%
+         */
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        float quarterly1 = 14;
+        float quarterly2 = 14;
+        float quarterly3 = 34;
+        float quarterly4 = 38;
+
+        yValues.add(new PieEntry(quarterly1, "Quarterly 1"));
+        yValues.add(new PieEntry(quarterly2, "Quarterly 2"));
+        yValues.add(new PieEntry(quarterly3, "Quarterly 3" ));
+        yValues.add(new PieEntry(quarterly4, "Quarterly 4"));
+
+        //y轴的集合
+        PieDataSet pieDataSet = new PieDataSet(yValues, "Quarterly Revenue 2014"/*显示在比例图上*/);
+        pieDataSet.setSliceSpace(0f); //设置个饼状图之间的距离
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        pieDataSet.setColors(colors);
+
+        pieDataSet.setValueTextColor(Color.BLACK);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float px = 5 * (metrics.densityDpi / 160f);
+        pieDataSet.setSelectionShift(px); // 选中态多出的长度
+        PieData pieData = new PieData(pieDataSet);
+        return pieData;
+    }
+
+    
 }
