f882d9baaa135ba0a3ce02ae581b0d914dee48d0
wdf <>
2019-05-04 11:47:11 +0800

    Merge branch 'master' of https://gitee.com/psvm_z/MonashFriendFinder
    # Conflicts:
    #	app/src/main/java/edu/monashsuzhou/friendfinder/activity/Login.java

--- app/src/main/java/edu/monashsuzhou/friendfinder/Constant.java
+++ app/src/main/java/edu/monashsuzhou/friendfinder/Constant.java
@@ -1,7 +1,8 @@
 package edu.monashsuzhou.friendfinder;
 
 public interface Constant {
-    String SEVER_HOST = "192.168.1.100";
+//    String SEVER_HOST = "192.168.1.100";
+    String SEVER_HOST = "192.168.0.106";
     String SEVER_PORT = "8080";
     String SEVER_NAME = "5183_A1";
 
