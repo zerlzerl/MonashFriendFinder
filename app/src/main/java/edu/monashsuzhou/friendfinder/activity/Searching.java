afd56cb4ca6dd3038eb7d6038abd8cf7d8a89710
zerl <zerlzerl@163.com>
2019-05-05 20:20:05 +0800

    searching

--- app/src/main/java/edu/monashsuzhou/friendfinder/activity/Searching.java
+++ app/src/main/java/edu/monashsuzhou/friendfinder/activity/Searching.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -30,21 +31,32 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import edu.monashsuzhou.friendfinder.R;
 
 import edu.monashsuzhou.friendfinder.MainActivity;
+import edu.monashsuzhou.friendfinder.entity.StudentProfile;
+import edu.monashsuzhou.friendfinder.util.HttpUtil;
+import edu.monashsuzhou.friendfinder.util.MD5Util;
+import edu.monashsuzhou.friendfinder.util.StringUtils;
 
 
 public class Searching extends AppCompatActivity {
     private static ViewPager viewPager;
 
-    private static String searchCriteria = null;
+    private static StudentProfile searchCriteria = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,7 +87,9 @@ public class Searching extends AppCompatActivity {
                 } else{
                     //切换到search页面的时候去做搜索和渲染工作
                     View currentView = viewPager.getChildAt(position);
-                    ((TextView)currentView.findViewById(R.id.search_result)).setText(Searching.searchCriteria);
+                    SearchAndRenderTask searchAndRenderTask = new SearchAndRenderTask();
+                    searchAndRenderTask.execute(Searching.searchCriteria);
+//                    ((TextView)currentView.findViewById(R.id.search_result)).setText(Searching.searchCriteria);
                 }
             }
 
@@ -328,22 +342,127 @@ public class Searching extends AppCompatActivity {
                 AlertDialog dialog = builder.create();
                 dialog.show();
             });
-            Log.i("onCreateView","跳转到FilterFragment");
+
+
+
+            // set search button
             Button switch2resultBtn = view.findViewById(R.id.searchBtn);
-            switch2resultBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-//                    Searching.searchCriteria = filterContent.getText().toString();
-                    InputMethodManager imm = (InputMethodManager) view.getContext()
-                            .getSystemService(Context.INPUT_METHOD_SERVICE);
-                    if( imm != null){
-                        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+            switch2resultBtn.setOnClickListener(view13 -> {
+
+                // 获取参数
+                // collection from the screen
+                // visible
+                String email = ((EditText)view.findViewById(R.id.filter_email_text)).getText().toString();
+                String fisrtName = ((EditText)view.findViewById(R.id.filter_firstName_text)).getText().toString();
+                String surname = ((EditText)view.findViewById(R.id.filter_Surname_text)).getText().toString();
+                String suburb = ((EditText)view.findViewById(R.id.filter_suburb_text)).getText().toString();
+
+                RadioButton male = (RadioButton)view.findViewById(R.id.filter_radio_male);
+                RadioButton female = (RadioButton)view.findViewById(R.id.filter_radio_female);
+                String gender = null;
+                if (male.isChecked()) gender = "male";
+                if (female.isChecked()) gender = "female";
+
+                // not sure for visibility
+                String dateOfBirth = null;
+                if (is_visible(view.findViewById(R.id.filter_dateOfBirth_layout))) {
+                    dateOfBirth = ((EditText) view.findViewById(R.id.filter_dateOfBirth)).getText().toString();
+                    if (StringUtils.isBlank(dateOfBirth)) {
+                        try {
+                            SimpleDateFormat df1 = new SimpleDateFormat("dd/MM/yyyy");
+                            Date date = df1.parse(dateOfBirth);
+                            SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd/HH:mm:ss");
+                            dateOfBirth = df2.format(date) + "+08:00";
+                            dateOfBirth = dateOfBirth.replace("/", "T");
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
                     }
-                    ViewPager viewPager = Searching.getViewPager();
-                    viewPager.setCurrentItem(1);
                 }
+
+                String nation = null;
+                String lang = null;
+                if (is_visible(view.findViewById(R.id.filter_nation_language_spinner_layout))) {
+                    nation = ((Spinner) view.findViewById(R.id.filter_nation_spinner)).getSelectedItem().toString();
+                    lang = ((Spinner) view.findViewById(R.id.filter_language_spinner)).getSelectedItem().toString();
+                }
+
+                String address = null;
+                if (is_visible(view.findViewById(R.id.filter_address_text_layout))) {
+                    address = ((EditText) view.findViewById(R.id.filter_address_text)).getText().toString();
+                }
+                String job = null;
+                if (is_visible(view.findViewById(R.id.filter_current_job_layout))) {
+                    job = ((EditText) view.findViewById(R.id.filter_current_job)).getText().toString();
+                }
+
+                String studyMode = null;
+                if (is_visible(view.findViewById(R.id.filter_radio_mode_layout))) {
+                    RadioButton part = (RadioButton) view.findViewById(R.id.filter_radio_part_time);
+                    RadioButton full = (RadioButton) view.findViewById(R.id.filter_radio_full_time);
+
+                    if (part.isChecked()) studyMode = "Part Time";
+                    if (full.isChecked()) studyMode = "Full Time";
+                }
+
+                String course = null;
+                if (is_visible(view.findViewById(R.id.course_multi_selector_layout))) {
+                    course = ((EditText) view.findViewById(R.id.filter_course_multi_selector)).getText().toString();
+                }
+                if (!StringUtils.isBlank(course)) course = course.substring(0,course.length()-1);
+
+                String favorUnit = null;
+                if (is_visible(view.findViewById(R.id.filter_favor_unit_spinner_layout))) {
+                    favorUnit = ((Spinner) view.findViewById(R.id.filter_favor_unit_spinner)).getSelectedItem().toString();
+                }
+
+                String favorSport = null;
+                if (is_visible(view.findViewById(R.id.filter_favor_sport_text_layout))) {
+                    favorSport = ((EditText) view.findViewById(R.id.filter_favor_sport_text)).getText().toString();
+                }
+
+                String favorMovie = null;
+                if (is_visible(view.findViewById(R.id.filter_favor_movie_text_layout))) {
+                    favorMovie = ((EditText) view.findViewById(R.id.filter_favor_movie_text)).getText().toString();
+                }
+
+                StudentProfile criteria = new StudentProfile()
+                        .setEmail(email)
+                        .setFirstName(fisrtName)
+                        .setSurname(surname)
+                        .setGender(gender)
+                        .setDateOfBirth(dateOfBirth)
+                        .setNationality(nation)
+                        .setNativeLanguage(lang)
+                        .setSuburb(suburb)
+                        .setAddress(address)
+                        .setCurrentJob(job)
+                        .setStudyMode(studyMode)
+                        .setCourse(course)
+                        .setFavouriteUnit(favorUnit)
+                        .setFavouriteSport(favorSport)
+                        .setFavouriteMovie(favorMovie);
+
+                Log.i("Search Criteria", JSON.toJSONString(criteria));
+
+                //设置查询条件
+                Searching.searchCriteria = criteria;
+
+                //关闭软键盘
+                InputMethodManager imm = (InputMethodManager) view13.getContext()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+                if( imm != null){
+                    imm.hideSoftInputFromWindow(view13.getWindowToken(),0);
+                }
+                ViewPager viewPager = Searching.getViewPager();
+                //跳转
+                viewPager.setCurrentItem(1);
             });
             return view;
+        }
+
+        private boolean is_visible(View view){
+            return (view.getVisibility() == View.VISIBLE);
         }
 
         private void goneCriteria(View view, int i) {
@@ -461,6 +580,32 @@ public class Searching extends AppCompatActivity {
                 default:
                     break;
             }
+        }
+    }
+
+    public static class SearchAndRenderTask extends AsyncTask<StudentProfile, Integer, String>{
+
+        @Override
+        protected String doInBackground(StudentProfile... studentProfiles) {
+            StudentProfile criteria = studentProfiles[0];
+            String search_result = null;
+            try {
+                search_result = HttpUtil.post("Profile", "findByAnyAttr", criteria);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (search_result != null){
+                //没有网络错误
+                JSONArray results = JSON.parseArray(search_result);
+                if (results.size() == 0){
+                    // 无符合条件的对象
+                } else {
+                    // 对结果进行渲染
+                    Log.i("results", JSON.toJSONString(results));
+                }
+            }
+            return null;
         }
     }
 }
