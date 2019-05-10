bb9a8dc21c6eb8672577d05c7da0501ad79751de
amoy_zhp <whitehawk15@hotmail.com>
2019-05-10 09:45:33 +0800

    update Constant

--- app/src/main/java/edu/monashsuzhou/friendfinder/Constant.java
+++ app/src/main/java/edu/monashsuzhou/friendfinder/Constant.java
@@ -2,7 +2,7 @@ package edu.monashsuzhou.friendfinder;
 
 public interface Constant {
 //    String SEVER_HOST = "192.168.1.100";
-    String SEVER_HOST = "47.102.97.239";
+    String SEVER_HOST = "192.168.1.105";
     String SEVER_PORT = "8080";
     String SEVER_NAME = "5183_A1";
 
@@ -12,4 +12,14 @@ public interface Constant {
     Integer N_DAYS = 1;
 
     String MOVIE_HOST = "http://www.omdbapi.com/?apikey=a98c3027&t=";
+
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME =
+            "com.zhp.googlemaptutorial.MapsActivity";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME +
+            ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME +
+            ".LOCATION_DATA_EXTRA";
 }
